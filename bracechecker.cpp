20d4130380133cb6b8ab70273f9c6ab8d99b820f
aleeexk <53943917+aleeexk@users.noreply.github.com>
2019-10-21 07:43:18 +0700

    Add files via upload

---
+++ bracechecker.cpp
@@ -0,0 +1,34 @@
+#include "bracechecker.h"
+#include "stack" 
+bracechecker::bracechecker(){};
+bool bracechecker::isbalanced(const string& data){
+    stack <int> st;
+    for (int i = 1; i < data.length(); i++){
+        if (data[i] != '(' && data[i] != '{' && data[i] != '[' && data[i] != ']' && data[i] != '}' && data[i] != ')') continue;
+        if (data[i] == '(') st.push(data[i]);
+        if (data[i] == '[') st.push(data[i]);
+        if (data[i] == '{') st.push(data[i]);
+        if (data[i] == ')'){
+            if (!st.empty()){
+                if(st.top() == '(') st.pop();
+                else return false;
+                }
+            else return false;
+        }
+        if (data[i] == ']'){
+            if (!st.empty()){
+                if(st.top() == '[') st.pop();
+                else return false;
+                }
+            else return false;
+        }
+        if (data[i] == '}'){
+            if (!st.empty()){
+                if(st.top() == '{') st.pop();
+                else return false;
+                }
+            else return false;
+        }
+    }
+    if (st.empty()) return true; else return false;
+}
---
+++ bracechecker.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+using namespace std;
+class bracechecker{
+    public:
+    bracecheker();
+    static bool isbalanced(string&);
+};
